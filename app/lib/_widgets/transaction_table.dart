5c2e45b42e618f83f69b9d5fe779c5467cbf87a0
joonashak <hakkijoo@gmail.com>
2022-07-12 01:34:59 +0300

    Add provider model and get some data from it

--- app/lib/_widgets/transaction_table.dart
+++ app/lib/_widgets/transaction_table.dart
@@ -1,7 +1,26 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class TransactionTable extends StatelessWidget {
   const TransactionTable({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+        create: (context) => TableData(), child: const Smth());
+  }
+}
+
+class TableData extends ChangeNotifier {
+  final List<String> _data = ['moi', 'hei'];
+
+  UnmodifiableListView<String> get data => UnmodifiableListView(_data);
+}
+
+class AcualTable extends StatelessWidget {
+  const AcualTable({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -10,5 +29,21 @@ class TransactionTable extends StatelessWidget {
     ], rows: const <DataRow>[
       DataRow(cells: <DataCell>[DataCell(Text('data'))])
     ]);
+  }
+}
+
+class Smth extends StatelessWidget {
+  const Smth({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TableData>(
+      builder: (context, value, child) {
+        return Column(
+          children: List<Text>.generate(
+              value.data.length, (index) => Text(value.data[index])),
+        );
+      },
+    );
   }
 }
